9502df4096f03bc2dff157a99bf926a3122219c7
marcos reyes <markosgr72@gmail.com>
2023-11-09 02:35:24 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express")
+const path = require("path")
+const app = express()
+const pathStatic = path.resolve(__dirname,"./public")
+
+app.use(express.static(pathStatic))
+const port = process.env.PORT || 1422;
+app.listen(port,()=>{
+    console.log(
+`Servidor corriendo en el puerto ${port}`);
+    })
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve("./views/login.html"))
+})
